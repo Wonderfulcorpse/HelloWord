b53d02472f508e676c21273b24155e3d64987472
unknown <429196-15>
2022-09-07 16:08:33 +0300

    ff

---
+++ Лаб1Задание1/Лаб1Задание1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Лаб1Задание1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = -1; //объявление переменной А и присваивание ей значение (сторона треугольника)
+            if (A >= 0)
+            {
+                double S = A * A * Math.Sqrt(3) / 4; //объявление переменной S
+                double R = A * Math.Sqrt(3) / 3; //объявление переменной R
+
+                Console.WriteLine("S = " + S); //вывод переменной S
+                Console.WriteLine("R = " + R); //вывод переменной R
+            }
+            else
+            {
+                Console.WriteLine("ОШИБКА!!! Сторона треугольника не имеет право быть отрицательной!!!");
+            }
+            Console.Read();
+        }
+    }
+}
